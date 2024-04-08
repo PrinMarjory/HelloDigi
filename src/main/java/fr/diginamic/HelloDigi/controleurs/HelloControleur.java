b0e1e11a9080ec35c41383fe5ec7da235232beb1
Marjory PRIN <marjory.prin@gmail.com>
2024-04-08 17:15:08 +0200

    TP2 - service

--- src/main/java/fr/diginamic/HelloDigi/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/HelloDigi/controleurs/HelloControleur.java
@@ -1,13 +1,19 @@
 package fr.diginamic.HelloDigi.controleurs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import fr.diginamic.HelloDigi.service.HelloService;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloControleur {
 	
+	@Autowired
+	private HelloService helloService;
+	
 	@GetMapping
 	public String direHello() {
-		return "Hello";
+		return helloService.salutations();		
 	}
 }
