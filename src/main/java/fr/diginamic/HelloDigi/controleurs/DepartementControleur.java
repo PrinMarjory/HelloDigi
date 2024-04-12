e9dec1d41e9f7186989a2437205e76115588bd23
Marjory PRIN <marjory.prin@gmail.com>
2024-04-12 11:40:17 +0200

    TP10 - export pdf et csv

--- src/main/java/fr/diginamic/HelloDigi/controleurs/DepartementControleur.java
+++ src/main/java/fr/diginamic/HelloDigi/controleurs/DepartementControleur.java
@@ -5,10 +5,14 @@ import fr.diginamic.HelloDigi.exception.FunctionalException;
 import fr.diginamic.HelloDigi.mapper.DepartementMapper;
 import fr.diginamic.HelloDigi.model.Departement;
 import fr.diginamic.HelloDigi.service.DepartementService;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,9 +24,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+
 @RestController
 @RequestMapping("/departement")
 public class DepartementControleur {
+	
+	@Value("${hellodigi.export}")
+	private String file;
 		
 	@Autowired
 	private DepartementService departementService;
@@ -43,6 +59,46 @@ public class DepartementControleur {
 	@GetMapping("/code/{code}")
 	public DepartementDTO getDepartementParCode(@PathVariable String code) {
 		return departementMapper.toDto(departementService.extractDepartement(code));
+	}
+	
+	@GetMapping("/code/{code}/export/pdf")
+	public void exportPDF(@PathVariable String code, HttpServletResponse response) throws IOException,DocumentException {
+		
+		DepartementDTO depart = departementMapper.toDto(departementService.extractDepartement(code));
+		Document docPdf = new Document(PageSize.A4);
+		
+		response.setHeader("Content-Disposition","attachment; filename=\""+code+"-"+file+".pdf\"");
+		
+		PdfWriter.getInstance(docPdf, response.getOutputStream());
+		
+		docPdf.open();
+		docPdf.addAuthor("Marjory PRIN");
+		docPdf.addTitle("Fiche département - "+depart.getNom());
+		docPdf.newPage();
+		BaseFont basefont = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.WINANSI, BaseFont.EMBEDDED);
+		Phrase p1 = new Phrase("Nom du département : "+depart.getNom()+" ("+depart.getCode()+")\n", new Font(basefont, 32.0f, 1, new BaseColor(0, 51, 88)));
+		Phrase p2 = new Phrase("Nombre d'habitants : "+depart.getNbHabitants()+" hab.\n", new Font(basefont, 32.0f, 1, new BaseColor(0, 51, 88)));
+		docPdf.add(p1);
+		docPdf.add(p2);
+		docPdf.close();
+		
+		response.flushBuffer();
+	}
+	
+	@GetMapping("/export/csv")
+	public void exportCSV(HttpServletResponse response) throws IOException {
+		
+		List<DepartementDTO> departements = departementService.extractDepartements().stream().map(dep -> departementMapper.toDto(dep)).toList();
+		
+		response.setContentType("text/csv");
+		response.setHeader("Content-Disposition","attachment; filename=\"Départements-"+file+".csv\"");
+		
+		PrintWriter writer = response.getWriter();
+		writer.println("Code département,Nom du département,Nombre d'habitants");
+		
+        for (DepartementDTO depart : departements) {
+            writer.println(depart.getCode() + "," + depart.getNom() + "," + depart.getNbHabitants());
+        }
 	}
 	
 	@PostMapping
--- src/main/java/fr/diginamic/HelloDigi/controleurs/VilleControleur.java
+++ src/main/java/fr/diginamic/HelloDigi/controleurs/VilleControleur.java
@@ -4,12 +4,16 @@ import fr.diginamic.HelloDigi.dto.VilleDTO;
 import fr.diginamic.HelloDigi.exception.FunctionalException;
 import fr.diginamic.HelloDigi.model.Ville;
 import fr.diginamic.HelloDigi.service.VilleService;
+import jakarta.servlet.http.HttpServletResponse;
 import fr.diginamic.HelloDigi.mapper.VilleMapper;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,9 +25,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+
 @RestController
 @RequestMapping("/villes")
 public class VilleControleur {
+	
+	@Value("${hellodigi.export}")
+	private String file;
 		
 	@Autowired
 	private VilleService villeService;
@@ -44,6 +60,48 @@ public class VilleControleur {
 	@GetMapping("nom/{nom}")
 	public VilleDTO getVilleParNom(@PathVariable String nom) {
 		return villeMapper.toDto(villeService.extractVille(nom));
+	}
+	
+	@GetMapping("nom/{nom}/export/pdf")
+	public void exportPDF(@PathVariable String nom, HttpServletResponse response) throws IOException,DocumentException {
+		
+		VilleDTO ville = villeMapper.toDto(villeService.extractVille(nom));
+		Document docPdf = new Document(PageSize.A4);
+		
+		response.setHeader("Content-Disposition","attachment; filename=\""+nom+"-"+file+".pdf\"");
+		
+		PdfWriter.getInstance(docPdf, response.getOutputStream());
+		
+		docPdf.open();
+		docPdf.addAuthor("Marjory PRIN");
+		docPdf.addTitle("Fiche ville - "+ville.getNom());
+		docPdf.newPage();
+		BaseFont basefont = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.WINANSI, BaseFont.EMBEDDED);
+		Phrase p1 = new Phrase("Nom de la ville : "+ville.getNom()+"\n", new Font(basefont, 32.0f, 1, new BaseColor(0, 51, 88)));
+		Phrase p2 = new Phrase("Nombre d'habitants : "+ville.getNbHabitants()+" hab.\n", new Font(basefont, 32.0f, 1, new BaseColor(0, 51, 88)));
+		Phrase p3 = new Phrase("Département : "+ville.getNomDepartement()+" ("+ville.getCodeDepartement()+")\n", new Font(basefont, 32.0f, 1, new BaseColor(0, 51, 88)));
+		docPdf.add(p1);
+		docPdf.add(p2);
+		docPdf.add(p3);
+		docPdf.close();
+		
+		response.flushBuffer();
+	}
+	
+	@GetMapping("/export/csv")
+	public void exportCSV(HttpServletResponse response) throws IOException {
+		
+		List<VilleDTO> villes = villeService.extractVilles().stream().map(ville -> villeMapper.toDto(ville)).toList();
+		
+		response.setContentType("text/csv");
+		response.setHeader("Content-Disposition","attachment; filename=\"Villes-"+file+".csv\"");
+		
+		PrintWriter writer = response.getWriter();
+		writer.println("Nom de la ville,Nombre d'habitants,Code département,Nom du département");
+		
+        for (VilleDTO ville : villes) {
+            writer.println(ville.getNom() + "," + ville.getNbHabitants() + "," + ville.getCodeDepartement() + "," + ville.getNomDepartement());
+        }
 	}
 	
 	@PostMapping
